d44734a71701e0a390ed43e8ebc65b7c5160a022
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-05-10 08:59:33 +0530

    Posting data from fetch

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -30,7 +30,7 @@ class CommentForm extends Component {
 
     handleSubmit(values){
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -108,7 +108,7 @@ class CommentForm extends Component {
 
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if (comments == null) {
             return (<div></div>)
         }
@@ -136,7 +136,7 @@ class CommentForm extends Component {
                 <ul className='list-unstyled'>
                     {comm}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
       
@@ -176,7 +176,7 @@ class CommentForm extends Component {
                 <div className="row">
                     <RenderDish  dish={props.dish}/>
                     <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id}
                     />
                 </div>
